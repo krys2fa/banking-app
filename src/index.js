a37eb5cdbc80629508c4a9b82688d2dc1ed86eb4
Christopher Amanor <kriskit7@gmail.com>
2021-03-24 03:13:54 +0000

    Add common.js file to validate fields

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { React } from "react";
-import { ReactDOM } from "react-dom";
-import { AppRouter } from "./router/AppRouter";
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./router/AppRouter";
 import { Provider } from 'react-redux';
 import store from './store/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
